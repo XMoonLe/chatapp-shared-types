8ab0304620b0400d0624e82fa48283bb0a531071
XMoon <xmoonle11@gmail.com>
2025-04-05 21:53:24 -0400

    deleted stuff

--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -1,2 +1,50 @@
-export * from "./data-ui";
-export * from "./socket-events";
+export type Message = {
+  userId: string;
+  username: string;
+  content: string;
+};
+
+export type User = {
+  userId: string;
+  username: string;
+  isTyping: boolean;
+};
+
+export interface ClientToServerEvents {
+  userConnected: {
+    username: string;
+    userId: string;
+  };
+  typing: {
+    userId: string;
+    username: string;
+  };
+  stopTyping: {
+    userId: string;
+    username: string;
+  };
+  chatMessage: {
+    username: string;
+    content: string;
+  };
+}
+
+export interface ServerToClientEvents {
+  connectedUsers: {
+    userId: string;
+    username: string;
+    ip: string;
+  }[];
+  typing: {
+    userId: string;
+    username: string;
+  };
+  stopTyping: {
+    userId: string;
+    username: string;
+  };
+  chatMessage: {
+    username: string;
+    content: string;
+  };
+}
